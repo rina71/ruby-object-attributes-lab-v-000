f36f6d0c5fbb7bb530788b446c37531fb46d90c6
Yurina Shin <yurinas@gmail.com>
2017-11-21 04:59:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def name
+    @name
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def breed
+    @breed
+  end
+end
